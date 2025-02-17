f5007f58184e7c50828b7ca9f73e34b8149ad92b
Clara Mesquita <clara.mesquita@aluno.uece.br>
2025-02-16 21:50:16 -0300

    feat: Add back-end version 1.2.0

---
+++ app/main/seeders/database_key_seeder.py
@@ -0,0 +1,37 @@
+from app.main import db
+from app.main.model import DatabaseKey
+from app.main.service import generate_keys
+
+
+def add_database_keys():
+    public_key_str, private_key_str = generate_keys()
+    database_keys = DatabaseKey(
+        database_id=1, public_key=public_key_str, private_key=private_key_str
+    )
+    db.session.add(database_keys)
+
+    public_key_str, private_key_str = generate_keys()
+    database_keys = DatabaseKey(
+        database_id=2, public_key=public_key_str, private_key=private_key_str
+    )
+    db.session.add(database_keys)
+
+    public_key_str, private_key_str = generate_keys()
+    database_keys = DatabaseKey(
+        database_id=3, public_key=public_key_str, private_key=private_key_str
+    )
+    db.session.add(database_keys)
+
+    public_key_str, private_key_str = generate_keys()
+    database_keys = DatabaseKey(
+        database_id=4, public_key=public_key_str, private_key=private_key_str
+    )
+    db.session.add(database_keys)
+
+    public_key_str, private_key_str = generate_keys()
+    database_keys = DatabaseKey(
+        database_id=5, public_key=public_key_str, private_key=private_key_str
+    )
+    db.session.add(database_keys)
+
+    db.session.flush()
